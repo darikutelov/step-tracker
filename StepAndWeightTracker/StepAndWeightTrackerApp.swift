55d1e513014bc684b3e91fcc44bf26f79eac777b
darikutelov <dari@kutelov.com>
2024-05-08 12:29:05 +0300

    Initial Commit

---
+++ StepAndWeightTracker/StepAndWeightTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepAndWeightTrackerApp.swift
+//  StepAndWeightTracker
+//
+//  Created by Dariy Kutelov on 8.05.24.
+//
+
+import SwiftUI
+
+@main
+struct StepAndWeightTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
